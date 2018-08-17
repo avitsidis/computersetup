81aa22923d2e8e5faf01d55787f4670b2ffca5c1
avitsidis <avitsidis@users.noreply.github.com>
2018-08-17 09:58:48 +0000

    add all base tools for a development computer

--- setup-base-dev.ps1
+++ setup-base-dev.ps1
@@ -1,23 +1,49 @@
+$principal = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+if(-not $isAdmin)
+{
+    Write-Warning "Script must be executed as Administrator"
+    Write-Warning "Cancel execution."
+    exit;
+}
 
 Write-Host "Install chocolatey"
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Install PS Get"
-(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+#modules
+Install-Module -Name posh-git
 
-Write-Host "install posh-git"
-install-module posh-git
-
+# tools
+choco install procexp -y
+choco install paint.net -y
+choco install keepass.install -y
 choco install 7zip.install -y
+choco install adobereader -y
+choco install notepadplusplus.install -y
 
+#Dev
+choco install python3 -y
+choco install nodejs.install -y
+choco install git.install -y
+choco install vscode -y
+choco install sourcetree -y
+choco install visualstudio2017enterprise -y
+#choco install visualstudio2015enterprise -y
+choco install resharper-platform -y
+
+
+#db
+choco install sql-server-management-studio -y
+choco install dbeaver -y
+
+#web
+choco install googlechrome -y
+choco install firefox -y
+choco install postman -y
+
+#network
+choco install wireshark -y
+choco install filezilla -y
 choco install putty -y
-
-write-host "configure Subsytem for linux"
-$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
-if (-not(Test-Path -Path $RegistryKeyPath)) {
-  New-Item -Path $RegistryKeyPath -ItemType Directory -Force
-}
-#New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
-Set-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
-
-Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -LimitAccess -NoRestart
+choco install fiddler -y
